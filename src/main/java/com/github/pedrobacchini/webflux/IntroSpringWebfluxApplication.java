57d1d1b638a8d28c12993a8ef39133e34a4bcc8b
Pedro Bacchini <pedrobacchini@outlook.com>
2019-06-13 10:03:53 -0300

    Clean First Commit

---
+++ src/main/java/com/github/pedrobacchini/webflux/IntroSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroSpringWebfluxApplication.class, args);
+    }
+
+}
