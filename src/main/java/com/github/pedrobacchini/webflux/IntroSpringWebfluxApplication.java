396bd9c1d3ced4ea8077b1a084781e109ed9d99b
Pedro Bacchini <pedrobacchini@outlook.com>
2019-06-13 10:13:52 -0300

    Configurando uri mongoDB

--- src/main/java/com/github/pedrobacchini/webflux/IntroSpringWebfluxApplication.java
+++ src/main/java/com/github/pedrobacchini/webflux/IntroSpringWebfluxApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IntroSpringWebfluxApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(IntroSpringWebfluxApplication.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(IntroSpringWebfluxApplication.class, args); }
 
 }
